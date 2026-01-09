651547c110134041c14c436df50ad36c32df69f8
Apsara GC <apsaragc29@gmail.com>
2026-01-09 10:38:59 +0545

    banking system

---
+++ banking-system/src/test/java/bank/banking_system/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
